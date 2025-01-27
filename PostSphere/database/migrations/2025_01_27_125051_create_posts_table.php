23e397ca951660187956eddd4d280a2f26b6cc67
ShineLynn <sla2482005@gmail.com>
2025-01-27 19:23:33 +0630

    second

--- PostSphere/database/migrations/2025_01_27_125051_create_posts_table.php
+++ PostSphere/database/migrations/2025_01_27_125051_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->string('category');
+            $table->string('image')->nullable();
+            $table->boolean('is_approved')->default(false);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
